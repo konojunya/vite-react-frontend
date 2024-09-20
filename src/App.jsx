148228887c0986e58ad48e88c20286cb5fffe239
JJ <junya03dance@gmail.com>
2024-09-20 17:00:19 +0900

    feat: add jsx

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+export const App = () => {
+  return <h1 className="text-red-900">きょーすけ</h1>;
+};
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import { createRoot } from "react-dom";
+import { App } from "./App.jsx";
+
+createRoot(document.querySelector("#app")).render(<App />);
